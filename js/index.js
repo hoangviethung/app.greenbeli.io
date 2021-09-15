4cf74095b21a7e40e8883000898b3674894631dc
Hoàng Việt Hùng <hoangviet.hung.009@gmail.com>
2021-09-15 22:50:41 +0700

    udpate unbox popup

--- js/index.js
+++ js/index.js
@@ -15,32 +15,63 @@ function fancyboxUnbox() {
     });
 }
 
-function showTokenNameUnbox() {
+function actionsSubmitUnbox() {
     $(".unbox_popup .popup__footer .bt--primary").on("click", function () {
-        const input = $(".unbox_popup__input .input-main input");
-        if (input.val() <= 0) {
-            input.addClass("is-invalid");
-            input
-                .siblings(".invalid-feedback")
-                .addClass("is-invalid")
-                .text("Vui lòng nhập số lượng");
-            $(".unbox_popup .token-name").removeClass("show");
-            $(".unbox_popup .successful-content").removeClass("show");
+        if ($(this).hasClass("js-validate-checkout")) {
+            const input = $(".unbox_popup__input .input-main input");
+            if (input.val() <= 0) {
+                input.addClass("is-invalid");
+                input
+                    .siblings(".invalid-feedback")
+                    .addClass("is-invalid")
+                    .text("Vui lòng nhập số lượng");
+                $(".unbox_popup .token-name").removeClass("show");
+                $(".unbox_popup .successful-content").removeClass("show");
+            } else {
+                input.removeClass("is-invalid");
+                input.siblings(".invalid-feedback").removeClass("is-invalid");
+                $(".unbox_popup .token-name").addClass("show");
+                $(".unbox_popup .successful-content").removeClass("show");
+                $(".unbox_popup .loading").addClass("show");
+                setTimeout(() => {
+                    $(".unbox_popup .loading").removeClass("show");
+                    $(".unbox_popup .successful-content").addClass("show");
+                    $(this)
+                        .text("Go to your tree")
+                        .removeClass("js-validate-checkout");
+                }, 3000);
+            }
         } else {
-            input.removeClass("is-invalid");
-            input.siblings(".invalid-feedback").removeClass("is-invalid");
-            $(".unbox_popup .token-name").addClass("show");
-            $(".unbox_popup .successful-content").removeClass("show");
-            $(".unbox_popup .loading").addClass("show");
+            location.href = "your-tree.html";
+        }
+    });
+}
+
+function limitBuyCharacter() {
+    $(".unbox_popup__input .input-range input").on("change", function () {
+        if ($(this).val() > 3) {
+            $(this).val("3");
+            $(".unbox_popup__input .input-main input").val($(this).val());
+            $(this)
+                .parents(".input-percent")
+                .find(".invalid-feedback")
+                .text("You can't buy more")
+                .addClass("is-invalid");
             setTimeout(() => {
-                $(".unbox_popup .loading").removeClass("show");
-                $(".unbox_popup .successful-content").addClass("show");
+                $(this)
+                    .parents(".input-percent")
+                    .find(".invalid-feedback")
+                    .text("")
+                    .removeClass("is-invalid");
             }, 3000);
+        } else {
+            $(".unbox_popup__input .input-main input").val($(this).val());
         }
     });
 }
 
 $(function () {
     fancyboxUnbox();
-    showTokenNameUnbox();
+    actionsSubmitUnbox();
+    limitBuyCharacter();
 });
