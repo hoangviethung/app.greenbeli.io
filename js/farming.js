4fd4b46174ac25b9471ff302442544eb06a615ea
Hoàng Việt Hùng <hoangviet.hung.009@gmail.com>
2021-09-28 21:07:17 +0700

    update

--- js/farming.js
+++ js/farming.js
@@ -65,10 +65,6 @@ function showFarmingItemDetail() {
     $(".farming__item .collapse_header").on("click", function (e) {
         e.preventDefault();
         e.stopPropagation();
-        $(".farming__item .collapse_header")
-            .not(this)
-            .parents(".farming__item")
-            .removeClass("active");
         $(this).parents(".farming__item").toggleClass("active");
     });
 }
